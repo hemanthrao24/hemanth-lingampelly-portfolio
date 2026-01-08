c910c1fe65fba7796271016e080036eac0449fa2
hemanthrao24 <hemanthlingampelly24@gmail.com>
2026-01-09 00:27:19 +0530

    Update script.js
    Updating changes

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', e => {
-        e.preventDefault();
-        document.querySelector(link.getAttribute('href'))
-            .scrollIntoView({ behavior: 'smooth' });
-    });
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".floating-navbar a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 150;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
 });
+
+
+
